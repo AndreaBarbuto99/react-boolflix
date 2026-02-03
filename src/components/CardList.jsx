a0c87d9fae8b81ba7c2d47687498aa8f93fe94ce
Andrea Barbuto <barbutoandrea99@gmail.com>
2026-02-03 20:50:30 +0100

    feat: added buttons to navigate between pages of API. Added reset of page when going back to default page or deleting search box

--- src/components/CardList.jsx
+++ src/components/CardList.jsx
@@ -4,10 +4,20 @@ import { useFilm } from "../contexts/FilmProvider";
 
 
 export default function CardList() {
-    const { films, filteredFilms, filteredSeries, search } = useFilm();
+    const { films, seriesMoviesCounter, popularCounter, setPopularCounter, setSeriesMoviesCounter, filteredFilms, filteredSeries, search } = useFilm();
+    const maxPopularPages = films.total_pages;
+    const maxFilmsPages = filteredFilms.total_pages;
+
+
 
     return (
         <>
+            <div className="buttons d-flex justify-content-evenly mb-3">
+                <button className="btn btn-dark" onClick={() => search === "" ? setPopularCounter(popularCounter - 1) : setSeriesMoviesCounter(seriesMoviesCounter - 1)}
+                    disabled={search === "" ? popularCounter === 1 : seriesMoviesCounter === 1}>Previous</button>
+                <button className="btn btn-dark" onClick={() => search === "" ? setPopularCounter(popularCounter + 1) : setSeriesMoviesCounter(seriesMoviesCounter + 1)}
+                    disabled={search === "" ? popularCounter === maxPopularPages : seriesMoviesCounter === maxFilmsPages}>Next</button>
+            </div>
             <div className="row justify-content-center gap-3">
                 {search === "" ? (
                     films.map(film => (
@@ -23,6 +33,12 @@ export default function CardList() {
                         ))}
                     </>
                 )}
+            </div>
+            <div className="buttons d-flex justify-content-around">
+                <button className="btn btn-dark" onClick={() => search === "" ? setPopularCounter(popularCounter - 1) : setSeriesMoviesCounter(seriesMoviesCounter - 1)}
+                    disabled={search === "" ? popularCounter === 1 : seriesMoviesCounter === 1}>Previous</button>
+                <button className="btn btn-dark" onClick={() => search === "" ? setPopularCounter(popularCounter + 1) : setSeriesMoviesCounter(seriesMoviesCounter + 1)}
+                    disabled={search === "" ? popularCounter === maxPopularPages : seriesMoviesCounter === maxFilmsPages}>Next</button>
             </div>
         </>
     );
--- src/contexts/FilmProvider.jsx
+++ src/contexts/FilmProvider.jsx
@@ -14,15 +14,17 @@ export function FilmProvider({ children }) {
     const [search, setSearch] = useState("");
     const [filteredFilms, setFilteredFilms] = useState([]);
     const [filteredSeries, setFilteredSeries] = useState([]);
+    const [popularCounter, setPopularCounter] = useState(1);
+    const [seriesMoviesCounter, setSeriesMoviesCounter] = useState(1);
 
 
     useEffect(() => {
-        axios.get(thirdEndpoint)
+        axios.get(`${thirdEndpoint}&page=${popularCounter}`)
             .then((res) => {
                 setFilms(res.data.results)
             })
             .catch((err) => console.error("Non è stato possibile eseguire la richiesta", err))
-    }, [])
+    }, [popularCounter])
 
 
     useEffect(() => {
@@ -32,19 +34,29 @@ export function FilmProvider({ children }) {
             return;
         }
 
-        const myUrlFilms = `${endpoint}&query=${search}`;
+        const myUrlFilms = `${endpoint}&query=${search}&page=${seriesMoviesCounter}`;
         axios.get(myUrlFilms)
             .then((res) => setFilteredFilms(res.data.results))
             .catch(console.error);
 
-        const myUrlSeries = `${secondEndpoint}&query=${search}`;
+        const myUrlSeries = `${secondEndpoint}&query=${search}&page=${seriesMoviesCounter}`;
         axios.get(myUrlSeries)
             .then((res) => setFilteredSeries(res.data.results))
             .catch(console.error);
+    }, [search, seriesMoviesCounter])
+
+    useEffect(() => {
+        if (search === "") {
+            setPopularCounter(1)
+        }
+        else {
+            setSeriesMoviesCounter(1)
+        }
+
     }, [search])
 
     return (
-        <FilmContext.Provider value={{ films, setSearch, search, filteredFilms, filteredSeries }}>
+        <FilmContext.Provider value={{ films, setSearch, popularCounter, seriesMoviesCounter, setPopularCounter, setSeriesMoviesCounter, search, filteredFilms, filteredSeries }}>
             {children}
         </FilmContext.Provider>
     )
